c684b53c045b1876432e82a1ffb74dee834060e4
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-02-09 20:02:17 -0300

    MongoDB Database

--- meetups/pages/api/new-meetup.js
+++ meetups/pages/api/new-meetup.js
@@ -1,9 +1,24 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+async const handler = (req, res) => {
   if (req.method === 'POST') {
     const data = req.body;
-    const { title, image, address, description } = data;
 
-    
+    const client = await MongoClient.connect(
+      'mongodb+srv://root:root@cluster0.gqup1.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({
+      message: 'Meetup inserted',
+    });
   }
 };
 
