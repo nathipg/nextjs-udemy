0b85ea1daea5d4b7a9f1841d35aa79d19940a9ec
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-02-08 21:37:12 -0300

    Linking between pages

--- my-app/pages/news/index.js
+++ my-app/pages/news/index.js
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/news-1">News 1</Link>
+        </li>
+        <li>
+          <Link href="/news/news-2">News 2</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
