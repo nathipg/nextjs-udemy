9afa68c6b4c4eac01edd143d11ec9a28d5554c2d
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-02-08 21:28:57 -0300

    Extracting Dynamic Route Data

--- my-app/pages/news/[id].js
+++ my-app/pages/news/[id].js
@@ -1,5 +1,10 @@
+import { useRouter } from 'next/router';
+
 const DetailPage = () => {
-  return <h1>Detail Page</h1>;
+  const router = useRouter();
+  const id = router.query.id;
+
+  return <h1>Detail Page: {id}</h1>;
 };
 
 export default DetailPage;
